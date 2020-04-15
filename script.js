d6ffe8880828095e9815ae8f1ba72b7f7cdcb9a1
bwhatnall <58062007+bwhatnall@users.noreply.github.com>
2020-04-15 18:10:15 +1200

    calculation added

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculateLatency(clock, CR) {
+    if ( clock < 1.0 ) {
+        clock = 1.0;
+    }
+    if ( CR < 1.0 ) {
+        CR = 1.0;
+    }
+
+    return ( 1.0/(clock/CR) * 2000.0 ); 
+}
+
+function calculateForm() {
+    let clock = Number(document.getElementById("clock").value);
+    let CR = Number(document.getElementById("CR").value);
+
+    let output = calculateLatency(clock, CR);
+
+    document.getElementById("output").innerHTML = output.toString().substr( 0, 10);
+}
