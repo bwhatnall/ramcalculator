363edcd24d5307b3ac0189128348915f4db8fe5f
bwhatnall <58062007+bwhatnall@users.noreply.github.com>
2020-04-15 18:12:53 +1200

    updated HTML with tags

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Function to calculate latency of RAM //
+
 function calculateLatency(clock, CR) {
     if ( clock < 1.0 ) {
         clock = 1.0;
@@ -9,10 +11,11 @@ function calculateLatency(clock, CR) {
     return ( 1.0/(clock/CR) * 2000.0 ); 
 }
 
+// Called upon function in HTML to get data and call calculateLatency function //
+
 function calculateForm() {
     let clock = Number(document.getElementById("clock").value);
     let CR = Number(document.getElementById("CR").value);
-
     let output = calculateLatency(clock, CR);
 
     document.getElementById("output").innerHTML = output.toString().substr( 0, 10);
